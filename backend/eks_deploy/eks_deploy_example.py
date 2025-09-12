7cddf8661276edb1a92641cd3963d8cacf538e04
zhangchg <zhangchg@amazon.com>
2025-09-12 01:49:48 +0000

    revert backend/eks_deploy/eks_deploy_example.py to complete code

--- backend/eks_deploy/eks_deploy_example.py
+++ backend/eks_deploy/eks_deploy_example.py
@@ -2,25 +2,20 @@ from eks_environment import EKSEnvironment
 from infrastructure import Infrastructure, InfrastructureOutput
 from vllm_model_deployer import VllmModelDeployer
 
-# stack = Infrastructure(cfn_tpl_file="./infrastructure.yaml")
-# output = stack.create_stack_and_wait_for_complete()
-# EKSEnvironment.prepare(
-#     eks_cluster_name=output.cluster_name,
-#     eks_node_role_arn=output.eks_cluster_node_role,
-#     eks_node_sg_id=output.application_security_group,
-#     eks_node_instance_type="g5.4xlarge",
-#     region=output.region
-# )
-
-
-# output = InfrastructureOutput()
-cluster_name = "perf-infplat-XP89R-eks"
-region = "us-west-2"
+stack = Infrastructure(cfn_tpl_file="./infrastructure.yaml")
+output = stack.create_stack_and_wait_for_complete()
+EKSEnvironment.prepare(
+    eks_cluster_name=output.cluster_name,
+    eks_node_role_arn=output.eks_cluster_node_role,
+    eks_node_sg_id=output.application_security_group,
+    eks_node_instance_type="g5.4xlarge",
+    region=output.region
+)
 
 host = VllmModelDeployer.deploy(
-    eks_cluster_name=cluster_name, # output.cluster_name,
+    eks_cluster_name=output.cluster_name,
     replicas=1,
-    region=region, # output.region,
+    region=output.region,
     vllm_model="Qwen/Qwen3-8B",
     tp_size=1,
     gpu_request=1,
